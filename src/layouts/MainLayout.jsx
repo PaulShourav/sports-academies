34758a7a5f8092c136dd81d4a20b2cc8a3fea3dd
Shourav paul <paul.shourav01@gmail.com>
2023-06-16 02:05:05 +0600

    footeer completed

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import { Toaster } from "react-hot-toast";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
     return (
         <>
             <Header />
             <Outlet />
+            <Footer/>
             <Toaster
                 position="top-right"
                 reverseOrder={false}
