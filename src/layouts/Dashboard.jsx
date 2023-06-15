60f8bc510b051de083ebdb1e8e1b35ee363e8665
Shourav paul <paul.shourav01@gmail.com>
2023-06-16 00:18:46 +0600

    Dashboard design updated

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,33 +1,42 @@
-import {Link, Outlet } from "react-router-dom";
+import {Link, NavLink, Outlet } from "react-router-dom";
 import useAdmin from "../hooks/useAdmin";
 import useInstructor from "../hooks/useInstructor";
 import useStudent from "../hooks/useStudent";
+import { AuthContext } from "../providers/AuthProvider";
+import { useContext } from "react";
+import { FaBookOpen, FaBookReader, FaHome, FaLaptopMedical, FaUsers } from "react-icons/fa";
 
 const Dashboard = () => {
+    const {user}=useContext(AuthContext)
     const [isAdmin]=useAdmin()
     const [isInstructor]=useInstructor()
     const [isStudent]=useStudent()
-    console.log(isAdmin);
+    console.log(user);
     const navLink = <>
-        
-        {isAdmin?<>
-            <li><Link to={'allUser'}>All user</Link></li>
-            <li><Link to={'allClasses'}>All Classes</Link></li>
-        </>:''}
+    <div className=" flex flex-col items-center mt-6">
+        <img src={user.photoURL} className=" w-24 h-24 rounded-full border-2 border-purple-900 " alt="" />
+        <p className="font-bold text-xl uppercase mt-2 mb-5 ">{user?.displayName}</p>
+    </div>
+    <li><NavLink className={({isActive})=>isActive?'active':''} to={'/'}><FaHome/> Home</NavLink ></li>
+    <hr className="border-t-[1px] border-black my-5"/>
+        {isAdmin && <>
+            <li><NavLink className={({isActive})=>isActive?'active':''} to={'allUser'}><FaUsers/> All user</NavLink ></li>
+            <li><NavLink className={({isActive})=>isActive?'active':''} to={'allClasses'}><FaBookReader/> All Classes</NavLink></li>
+        </>}
         {isInstructor?<>
-            <li><Link to={'addClass'}>Add Class</Link></li>
-            <li><Link to={'myClasses'}>My Classes</Link></li>
+            <li><NavLink className={({isActive})=>isActive?'active':''} to={'addClass'}><FaLaptopMedical/> Add Class</NavLink></li>
+            <li><NavLink className={({isActive})=>isActive?'active':''} to={'myClasses'}><FaBookOpen/> My Classes</NavLink></li>
         </>:''}
         {isStudent?<>
-            <li><Link to={'selectedClass'}>Selected Class</Link></li>
-            <li><Link to={'myClasses'}>My Classes</Link></li>
+            <li><NavLink className={({isActive})=>isActive?'active':''} to={'selectedClass'}><FaBookOpen/> Selected Class</NavLink></li>
+            <li><NavLink className={({isActive})=>isActive?'active':''} to={'myClasses'}>My Classes</NavLink></li>
         </>:''}
         {/* <li><Link to={'addClass'}>Add Class</Link></li></> */}
         </>
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="md:p-8 drawer-content flex flex-col ">
+            <div className="md:p-8 drawer-content flex flex-col items-center ">
                 {/* Page content here */}
                 <div className="">
 
@@ -37,7 +46,7 @@ const Dashboard = () => {
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                <ul className="menu  p-5  w-80 h-full bg-fuchsia-100 text-base-content">
                     {/* Sidebar content here */}
                     {navLink}
                 </ul>
